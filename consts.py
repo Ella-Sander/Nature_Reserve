2f7f62565816a40f12e0d5cef6c890243f0cc1b5
Ella-Sander <ellasanderella@gmail.com>
2022-10-26 01:20:47 +0300

    update

--- consts.py
+++ consts.py
@@ -1,1 +1,7 @@
-print("")
+import datetime
+
+CLEAN_FREQUENCY = datetime.timedelta(1)
+
+NATURE_RESERVES_FILE = r"C:\Users\ellas\PycharmProjects\Nature_Reserve\Nature_Reserve\Nature_reserves.csv"
+MAX_REPORTS = 5
+
