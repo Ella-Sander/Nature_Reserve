1fa8b3702344ef6d24860211e8cc8de948853827
Ella-Sander <ellasanderella@gmail.com>
2022-10-26 10:06:10 +0300

    add

--- consts.py
+++ consts.py
@@ -2,7 +2,7 @@ import datetime
 
 CLEAN_FREQUENCY = datetime.timedelta(1)
 
-NATURE_RESERVES_FILE = r"C:\Users\ellas\PycharmProjects\Nature_Reserve\Nature_Reserve\Nature_reserves.csv"
+NATURE_RESERVES_FILE = r"Nature_reserves.csv"
 MAX_REPORTS = 5
 
 
